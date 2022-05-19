8c394753096ac740bbb3afb36493f288d6e312e0
andrehideki <andre.hkiw.dev@gmail.com>
2022-05-19 10:58:19 -0300

    fix: should be able to update repository

--- src/index.js
+++ src/index.js
@@ -32,11 +32,12 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
+
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
